949f6afcfd654a92115f85cbb661f898b1119d95
savanidhruvin <dhruvinsavani9313@gmail.com>
2025-03-21 12:50:10 +0530

    savani21-3

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './Sidebar.css'
 
 const Sidebar = () => {
   return (
     <div>
-      
+        <div className='ds_side_bg'>
+
+        </div>
     </div>
   )
 }
