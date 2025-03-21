e767ed72cbc13c706a1241a0029fe859099ee38a
ParthVaghasiyakal-072 <parthvaghasiya.kalthiyainfo@gmail.com>
2025-03-21 12:50:49 +0530

    Parth21-03_1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Router, Routes, Route, Link } from "react-router-dom";
 import Layout from "./Components/Layout";
+import Login from "./Components/Login";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/layout" element={<Layout />}></Route>
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
---
+++ src/Components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../CSS/login.css';
+
+const Login = () => {
+  return (
+   <>
+   <section className='V_login_back'>
+
+   </section>
+   </>
+  )
+}
+
+export default Login
