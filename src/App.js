5fd705a8504858294d09536d591b74dba6663eb1
savanidhruvin <dhruvinsavani9313@gmail.com>
2025-03-24 17:57:28 +0530

    savani24-03-2025

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Router, Routes, Route, Link } from "react-router-dom";
 import Layout from "./Components/Layout";
 import Login from "./Components/Login";
+import Dashboard from "./Pages/Dashboard";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/layout" element={<Layout />} />
+        <Route path="/layout" element={<Layout />} >
+           <Route path="dashboard" element={<Dashboard/>} />
+        </Route>
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -14,6 +14,15 @@ import genre from '../Images/dhruvin/genre.svg'
 import book from '../Images/dhruvin/book.svg'
 import cast from '../Images/dhruvin/cast.svg'
 import review from '../Images/dhruvin/Review.svg'
+import episode from '../Images/dhruvin/Episode.svg'
+import unlock from '../Images/dhruvin/unlock.svg'
+import state from '../Images/dhruvin/state.svg'
+import playmaster from '../Images/dhruvin/master.svg'
+import song from '../Images/dhruvin/song.svg'
+import home from '../Images/dhruvin/home.svg'
+import label from '../Images/dhruvin/label.svg'
+import join from '../Images/dhruvin/join.svg'
+
 import { Offcanvas } from 'react-bootstrap'
 
 const Sidebar = ({offToggle , setOffToggle }) => {
@@ -150,6 +159,78 @@ const Sidebar = ({offToggle , setOffToggle }) => {
                    </div>
                  </div>
 
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={episode} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Episodes</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={unlock} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Episode Unlock</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={state} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Episode State</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={playmaster} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Playlist Master</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={song} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Playlist Songs</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={home} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Home Corousel</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={label} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Home Labels</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={join} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Home Label Join</h5>
+                   </div>
+                 </div>
+
               </div>
            </div>
         </div>
@@ -161,8 +242,206 @@ const Sidebar = ({offToggle , setOffToggle }) => {
            <Offcanvas.Title className='text-light'>LOGO</Offcanvas.Title>
          </Offcanvas.Header>
          <Offcanvas.Body>
-           Some text as placeholder. In real life you can have the elements you
-           have chosen. Like, text, images, lists, etc.
+         <div className='mt-2'>
+                 <div className=' ms-3 ds_active_pad  ds_active_color'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={dashboard} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Dashboard</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={role} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Role</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={king} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Subscription</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={coin} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Coin Label</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={master} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Coin Master</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={person} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>User Master</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={sub} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Subscription Sell</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={sell} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Coin Sell</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={voucher} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Vouchers</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={used} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Vouchers Used</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={genre} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Genre</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={book} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Audio Book</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={cast} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Cast Crew</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={review} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Review</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={episode} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Episodes</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={unlock} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Episode Unlock</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={state} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Episode State</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={playmaster} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Playlist Master</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={song} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Playlist Songs</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={home} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Home Corousel</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={label} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Home Labels</h5>
+                   </div>
+                 </div>
+
+                 <div className=' ms-3 ds_active_pad  py-2 mt-3'>
+                   <div className='d-flex align-items-center  ms-xl-5 ms-3'>
+                     <div>
+                        <img src={join} className="ds_side_icon" />
+                     </div>
+                     <h5 className='ms-3 text-light mb-0 ds_side_title'>Home Label Join</h5>
+                   </div>
+                 </div>
+
+              </div>
          </Offcanvas.Body>
       </Offcanvas>
     </div>
